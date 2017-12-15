1c52c160c5cb7bb412818d33d9a807ee0ada2a20
Jakub Wyczesany <ghaelios@gmail.com>
2017-12-15 12:50:24 +0100

    Adding first variables to JS

--- 1_Slider/js/app.js
+++ 1_Slider/js/app.js
@@ -1,3 +1,17 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function(){
+    var nextButton = document.querySelector("#nextPicture");
+    var prevButton = document.querySelector("#prevPicture");
+    var list = document.querySelectorAll("ul li");
+    function getList(elements) {
+        var arrList = [];
+        for(var i=0; i<elements.length; i++) {
+            arrList.push(elements[i]);
+        }
+        return arrList
+    }
+    var imageList = getList(list);
+    var selectedImage = imageList[0];
+    console.log(selectedImage);
+
+
+});
