78b09b20bec3e89d5bdae79ba9c4da913662b599
Jakub Wyczesany <ghaelios@gmail.com>
2017-12-26 17:03:14 +0100

    Gallery done

--- 2_Gallery/js/app.js
+++ 2_Gallery/js/app.js
@@ -1,7 +1,31 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function() {
 
+    var body = document.querySelector("body");
+    var list = document.querySelector("ul");
+    var listElements = list.querySelectorAll("li");
+    var newDiv = document.createElement("div");
+    var img = document.createElement("img");
+
+    for(var i=0; i<listElements.length; i++) {
+        listElements[i].addEventListener("click", function() {
+            console.log(this.innerHTML);
+            body.appendChild(newDiv);
+            newDiv.classList.add("fullScreen");
+            newDiv.appendChild(img);
+            img.setAttribute("src", this.firstElementChild.getAttribute("src"));
+            var button = document.createElement("button");
+            newDiv.appendChild(button);
+            button.innerText = "ZAMKNIJ";
+            button.classList.add("close");
+            button.addEventListener("click", function (event) {
+               newDiv.parentElement.removeChild(newDiv);
+               button.parentElement.removeChild(button);
+            });
+        });
+    }
+
+
+});
 /*
  <div class="fullScreen">
    <img src="./images/wrong.png">
--- 3_GalleryWithFilter/js/app.js
+++ 3_GalleryWithFilter/js/app.js
@@ -1,3 +1,4 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function() {
+    
+
+});
