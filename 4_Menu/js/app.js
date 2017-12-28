4b3e7e30f7461519057a8ffda39f1d6a477e2781
Jakub Wyczesany <ghaelios@gmail.com>
2017-12-28 16:08:23 +0100

    Finished menu !

--- 4_Menu/js/app.js
+++ 4_Menu/js/app.js
@@ -1,26 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Działa !");
     var list = document.querySelector("div ul");
-    console.log(list);
-    var firstSubMenu = list.children[1].querySelectorAll("li");
-    console.log(firstSubMenu);
-    var secondSubMenu = list.children[3].querySelectorAll("li");
-    console.log(secondSubMenu);
-    var openFirstMenu = list.children[1].querySelector("ul");
-    var openSecondMenu = list.children[3].querySelector("ul");
-    console.log(openFirstMenu, openSecondMenu);
+    var listEl = list.children;
 
-    firstSubMenu.addEventListener("mouseover", function () {
-        openFirstMenu.classList.add("nav");
-    });
-    firstSubMenu.addEventListener("mouseout", function () {
-        openFirstMenu.classList.remove("nav");
-    });
-
-    secondSubMenu.addEventListener("mouseover", function () {
-        openSecondMenu.classList.add("nav");
-    });
-    secondSubMenu.addEventListener("mouseout", function () {
-        openSecondMenu.classList.remove("nav");
-    });
+    for (var i = 0; i < listEl.length; i++) {
+        listEl[i].addEventListener("mouseover", function () {
+            if (this.querySelector("ul") != null) {
+                this.querySelector("ul").style.display = "inline-block";
+            }
+        });
+        listEl[i].addEventListener("mouseout", function () {
+            if (this.querySelector("ul") != null) {
+                this.querySelector("ul").style.display = "none";
+            }
+        });
+    }
 });
