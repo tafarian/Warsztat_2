f663f760c65983b114e0f289af0fa70d72b7abf6
Jakub Wyczesany <ghaelios@gmail.com>
2017-12-28 17:01:11 +0100

    Working tooltips

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -1,8 +1,20 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Działa !");
+    var tooltip = document.querySelectorAll(".tooltip");
+    console.log(tooltip);
 
+    for (var i = 0; i < tooltip.length; i++) {
+        tooltip[i].addEventListener("mouseover", function () {
+            var popup = document.createElement("span");
+            popup.className = "tooltipText";
+            var popupText = document.createTextNode(this.dataset.text);
+            popup.appendChild(popupText);
+            this.appendChild(popup);
+        });
+        tooltip[i].addEventListener("mouseout", function () {
+            var toDelete = document.querySelector(".tooltipText");
+            toDelete.parentElement.removeChild(toDelete);
+        });
+    }
 
-/*
- <span class="tooltipText">Text tooltipa</span>
- */
+});
