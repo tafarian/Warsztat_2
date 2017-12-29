0e57234e05901b225d5fcf193125c216e214ba27
Jakub Wyczesany <ghaelios@gmail.com>
2017-12-29 18:07:09 +0100

    ToDoList not finished

--- 6_ToDoList/js/app.js
+++ 6_ToDoList/js/app.js
@@ -1,9 +1,25 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function () {
+    var taskInput = document.querySelector("#taskInput");
+    var taskButton = document.querySelector("#addTaskButton");
+    var taskList = document.querySelector("#taskList");
+    var removeTask = document.querySelector("#removeFinishedTasksButton");
+    // console.log(taskInput, taskButton, taskList, removeTask);
+
+    taskButton.addEventListener("click", function() {
+        // console.log("klik");
+        var newTask = document.createElement("li");
+        taskList.appendChild(newTask);
+    });
+
+    removeTask.addEventListener("click", function() {
+        var toDelete = document.querySelector("#taskList > li");
+        toDelete.parentElement.removeChild(toDelete);
+
+    });
 
 
 
+});
 
 
 
