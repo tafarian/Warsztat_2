42b4871fc1d40d310401bf4697a543c35837dadb
Jakub Wyczesany <ghaelios@gmail.com>
2017-12-30 16:43:10 +0100

    ToDoList almost done

--- 6_ToDoList/js/app.js
+++ 6_ToDoList/js/app.js
@@ -7,8 +7,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     taskButton.addEventListener("click", function() {
         // console.log("klik");
+        var text = taskInput.value;
         var newTask = document.createElement("li");
+        var header = document.createElement("h1");
+        var taskText = document.createTextNode(text);
+        newTask.appendChild(taskText);
+        var newButtonDel = document.createElement("BUTTON");
+        var newButtonCom = document.createElement("BUTTON");
+        newButtonDel.innerText = "Delete";
+        newButtonCom.innerText = "Complete";
         taskList.appendChild(newTask);
+        newTask.setAttribute("class", "unDone");
+        newTask.appendChild(header);
+        header.appendChild(taskText);
+        newTask.appendChild(newButtonDel);
+        newButtonDel.addEventListener("click", function() {
+            newTask.parentElement.removeChild(newTask);
+        });
+        newTask.appendChild(newButtonCom);
+        newButtonCom.addEventListener("click", function() {
+            if (newTask.classList = "unDone") {
+                newTask.setAttribute("class", "done");
+            } else if (newTask.classList = "done") {
+                newTask.setAttribute("class", "unDone");
+            }
+        });
+        taskInput.value="";
     });
 
     removeTask.addEventListener("click", function() {
